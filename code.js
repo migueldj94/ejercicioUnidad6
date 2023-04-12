7dfdbc655a578c6933b43c979eca16a64889f66d
migue saavedra <125587415+migueldj94@users.noreply.github.com>
2023-04-12 14:48:29 -0500

    segundo commit en code js

---
+++ code.js
@@ -0,0 +1,16 @@
+//funciones 
+
+// declarativas 
+
+function miFuncion(){
+  return 3;   
+}
+
+//expresion 
+
+var otraFuncion =function(){
+    return a +b;
+}
+
+var nombre =(" hola mundo ")
+console.log 
