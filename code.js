a36ca85edaf34d845f6640478e065c21f358662d
migue saavedra <125587415+migueldj94@users.noreply.github.com>
2023-04-12 15:46:02 -0500

    cambios al archivo con ejercicios 2 y 3

--- code.js
+++ code.js
@@ -14,3 +14,28 @@ var otraFuncion =function(){
 
 var nombre =(" hola mundo ")
 console.log 
+
+var nombre ="miguel";
+var edad = 28;
+console.log(nombre,edad);
+
+//Definición variable local.
+let musica="Rock"
+console.log("Variable música antes del bloque:", musica)
+
+//Definición variable global
+var pelicula="Interestelar"
+//Para concatenar el texto con una variable / constante se utiliza la coma, como se ve a continuación
+console.log("Variable película antes del bloque:", pelicula)
+
+//esto es un bloque
+{
+  var pelicula="Matilda"
+  console.log("Variable película en el bloque:", pelicula)
+
+  let musica="Salsa"
+  console.log("Variable música en el bloque:", musica)
+}
+
+console.log("Variable película despues del bloque:", pelicula)
+console.log("Variable música despues del bloque:", musica)
