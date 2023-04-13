380706fe197d778e5f74eb9fc1dd545935cbc79b
migue saavedra <125587415+migueldj94@users.noreply.github.com>
2023-04-13 14:44:41 -0500

    ejercicios de estructuras de control

---
+++ estructurasControl.js
@@ -0,0 +1,9 @@
+/*if/else
+Permite que se realice una instrucción u otra de acuerdo a la evaluación de una condición. Su estructura es:
+    if(condicion){
+        instrucciones si la condicion se cumple
+    }else{
+        instrucciones si la condicion no se cumple
+    }
+En la condicion se utilizan operadores de comparación.
+*/
