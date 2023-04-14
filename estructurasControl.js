e9310011818a377a9205866a986fcdc321620d32
migue saavedra <125587415+migueldj94@users.noreply.github.com>
2023-04-14 14:29:43 -0500

    utimo ejeccio

--- estructurasControl.js
+++ estructurasControl.js
@@ -124,6 +124,18 @@ console.log(esMayor)
 
 
 
+/*For
+Permite ejecutar un código un número determinado de veces.
+Como se ve a continuación*/
+
+/*La variable i aumenta de uno en uno siempre que sea mejor que 10*/
+
+console.log("-----------------Condicional for----------------------")
+
+for (let i = 0; i < 10; i++) {
+    console.log(`El for va en ${i}`)
+
+
 var num2 =7;
 
 if (numero %2==0){
@@ -131,6 +143,22 @@ console.log("numero par");
 }else {
 console.log("Numero impar");
 }
+
+/*While.
+Permite ejecutar un código mientras se cumpla una condición.
+Como se ve a continuación*/
+
+console.log("-----------------Condicional while----------------------")
+
+let cont=0
+
+/*La variable cont aumenta en uno. Sigue siempre y cuando el contador sea menor que 10*/
+
+while(cont<10){
+    console.log(`El while va en ${cont}`)
+    cont++
+}
+
 
 
 contador = 1;
