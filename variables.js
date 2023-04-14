769a4a064e5ef73c2667c817edb72300ab367992
migue saavedra <125587415+migueldj94@users.noreply.github.com>
2023-04-14 18:20:36 -0500

    ejercicios de variables

--- variables.js
+++ variables.js
@@ -1,2 +1,26 @@
 var PI = 3.1416
 console.log(`este es el valor de ${PI}`)
+
+var nombre ="miguel";
+var edad = 28;
+console.log(`I am ${nombre}, and my ege ${edad}`)
+
+
+    var nombre = "miguel";
+    var apellido = "beltran";
+        console.log(`variable global antes del bloque, ${nombre}`)
+        console.log(`varible global antes del bloque,  ${apellido}`)
+
+    
+
+    {
+
+    let  metodo ="edad";
+    let  persona = "comida";
+    console.log(`variable local dentro del bloque, ${metodo}`)
+  }
+console.log(`variable despues del bloque, ${metodo}`)
+console.log(`variabel despues de bloque, ${persona}`)
+
+
+
