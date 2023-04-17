aeaad34e846fd5eca701d0cc3f1ec1b3a9dc8912
migue saavedra <125587415+migueldj94@users.noreply.github.com>
2023-04-17 16:02:55 -0500

    actualizacion de la guia

--- funciones.js
+++ funciones.js
@@ -36,3 +36,43 @@ function carrera(nombre="desconocido", carrera="desconocido"){
 
 carrera("Juan", "desarrollo de web")
 carrera(); //Cuando no se agrega parametro muestra el valor asignado en la funcion
+
+
+
+/*Funciones expresadas o anonimas.
+Las funciones expresadas hacen parte del ámbito concreto del programa, es decir, estas funciones no existen hasta que la expresión que las genera es ejecutada. Se consideran una buena practica de programación para tener el código ordenado*/
+
+console.log("--------------------Función Expresada-----------------------");
+const funcionExpresada = function(){
+    return console.log("Esta es una función expresada!");
+}
+
+funcionExpresada();
+
+/*Funciones anonimas autoejecutables (IIFE).
+Son funciones que se ejecutan al momento de crearlas. Su sintaxis es la siguiente:
+(function (parametros){//contenido de la funcion})(parametros);
+*/
+
+console.log("----------Funciones anonimas autoejecutables (IIFE)-------------");
+
+(function (){
+    console.log("Mi primera funcion anonima autoejecutable (IIFE)");
+})();
+
+(function (c){
+    c.log("Mi segunda funcion anonima autoejecutable (IIFE) con parametros");
+})(console);
+
+
+const Expresada= function(a,b){
+    if (a>b){
+    return 1;
+    }
+    else if (a<b){
+    return -1;
+    }else{
+    return 0;
+    }
+    }
+    console.log(funcionExpresada(6,5));
